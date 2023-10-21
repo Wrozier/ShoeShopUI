8689a3d2c47ba5af118cc36b30f2ed38a6d7785a
wrozier <williamrozier@icloud.com>
2023-10-21 19:17:19 -0400

    Added Product Model , Cart model

---
+++ ShoeShopUI/Models/Cart.swift
@@ -0,0 +1,18 @@
+//
+//  Cart.swift
+//  ShoeShopUI
+//
+//  Created by Will Rozier on 10/21/23.
+//
+
+import SwiftUI
+
+struct Cart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cart()
+}
