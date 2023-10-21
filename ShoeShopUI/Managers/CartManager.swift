2b6604f56267b43612baadafbe75a5cadf87037e
wrozier <williamrozier@icloud.com>
2023-10-21 18:39:42 -0400

    ShoeShop Ecommerce

---
+++ ShoeShopUI/Managers/CartManager.swift
@@ -0,0 +1,8 @@
+//
+//  CartManager.swift
+//  ShoeShopUI
+//
+//  Created by Will Rozier on 10/21/23.
+//
+
+import Foundation
---
+++ ShoeShopUI/Managers/PaymentHandler.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentHandler.swift
+//  ShoeShopUI
+//
+//  Created by Will Rozier on 10/21/23.
+//
+
+import Foundation
