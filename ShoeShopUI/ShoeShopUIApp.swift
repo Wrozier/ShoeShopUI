b7e478fc65f5793753b5b6451f2ac8f410cd8521
wrozier <williamrozier@icloud.com>
2023-10-21 17:45:27 -0400

    ShoeShop Ecommerce

---
+++ ShoeShopUI/ShoeShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeShopUIApp.swift
+//  ShoeShopUI
+//
+//  Created by Will Rozier on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoeShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
