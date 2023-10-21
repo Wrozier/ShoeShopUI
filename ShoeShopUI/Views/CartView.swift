a2876f3d3659c3d9a459964f9d5eddefe3623ec9
wrozier <williamrozier@icloud.com>
2023-10-21 19:15:15 -0400

    Added ViewGroup files

---
+++ ShoeShopUI/Views/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  ShoeShopUI
+//
+//  Created by Will Rozier on 10/21/23.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
